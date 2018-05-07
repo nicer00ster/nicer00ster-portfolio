509dba358e6bcd05176487db2fd49bc4be8c5a21
Alex Busch <abusch1013@gmail.com>
2018-05-07 16:22:40 -0400

    Add files via upload

---
+++ static/scripts/svgimports.js
@@ -0,0 +1,54 @@
+import ReactSVG from 'svg-react-loader?name=ReactSVG!../../static/images/svg/landing/react.svg';
+import Redux from 'svg-react-loader?name=Redux!../../static/images/svg/landing/redux.svg';
+import Express from 'svg-react-loader?name=Express!../../static/images/svg/landing/express.svg';
+import Node from 'svg-react-loader?name=Node!../../static/images/svg/landing/nodejs.svg';
+import Javascript from 'svg-react-loader?name=Javascript!../../static/images/svg/landing/javascript.svg';
+import Firebase from 'svg-react-loader?name=Firebase!../../static/images/svg/landing/firebase.svg';
+import MongoDB from 'svg-react-loader?name=MongoDB!../../static/images/svg/landing/mongodb.svg';
+import Pug from 'svg-react-loader?name=Pug!../../static/images/svg/landing/pug.svg';
+import Sketch from 'svg-react-loader?name=Sketch!../../static/images/svg/landing/sketch.svg';
+import Django from 'svg-react-loader?name=Django!../../static/images/svg/landing/django.svg';
+import Aws from 'svg-react-loader?name=Aws!../../static/images/svg/landing/aws.svg';
+import Es6 from 'svg-react-loader?name=Es6!../../static/images/svg/landing/es6.svg';
+import Html from 'svg-react-loader?name=Html!../../static/images/svg/landing/html-5.svg';
+import Python from 'svg-react-loader?name=Python!../../static/images/svg/landing/python.svg';
+import Webpack from 'svg-react-loader?name=Webpack!../../static/images/svg/landing/webpack.svg';
+import Npm from 'svg-react-loader?name=Npm!../../static/images/svg/landing/npm-2.svg';
+import AwsHub from 'svg-react-loader?name=AwsHub!../../static/images/svg/landing/aws-mobilehub.svg';
+import Nginx from 'svg-react-loader?name=Nginx!../../static/images/svg/landing/nginx.svg';
+import Bootstrap from 'svg-react-loader?name=Bootstrap!../../static/images/svg/landing/bootstrap.svg';
+import Babel from 'svg-react-loader?name=Babel!../../static/images/svg/landing/babel.svg';
+import Github from 'svg-react-loader?name=Github!../../static/images/svg/landing/github-icon.svg';
+import NextJS from 'svg-react-loader?name=NextJS!../../static/images/svg/landing/nextjs.svg';
+import Sass from 'svg-react-loader?name=Sass!../../static/images/svg/landing/sass.svg';
+
+
+const svg = {
+  'React': <ReactSVG width={50} height={50}/>,
+  'Redux': <Redux width={50} height={50}/>,
+  'Python': <Python width={50} height={50}/>,
+  'Express': <Express width={50} height={50} />,
+  'Javascript': <Javascript width={50} height={50} />,
+  'Firebase': <Firebase width={50} height={50} />,
+  'MongoDB': <MongoDB width={50} height={50} />,
+  'Pug': <Pug width={50} height={50} />,
+  'Sketch': <Sketch width={50} height={50} />,
+  'Django': <Django width={50} height={50} />,
+  'Aws': <Aws width={50} height={50} />,
+  'Es6': <Es6 width={50} height={50} />,
+  'Html': <Html width={50} height={50} />,
+  'Python': <Python width={50} height={50} />,
+  'Webpack': <Webpack width={50} height={50} />,
+  'Npm': <Npm width={50} height={50} />,
+  'AwsHub': <AwsHub width={50} height={50} />,
+  'Nginx': <Nginx width={50} height={50} />,
+  'Bootstrap': <Bootstrap width={50} height={50} />,
+  'Node': <Node width={50} height={50} />,
+  'Babel': <Babel width={50} height={50} />,
+  'Github': <Github width={50} height={50} />,
+  'NextJS': <NextJS width={50} height={50} />,
+  'Sass': <Sass width={50} height={50} />,
+
+}
+
+export default svg;
