deb7813633a4c756c9232fef502e2621ada978b1
Alex Busch <abusch1013@gmail.com>
2018-06-21 12:39:56 -0400

    Condensed NProgress into partial

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,4 @@
 import Link from './Link';
-import Head from 'next/head';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import Menu from './Menu';
@@ -27,48 +26,43 @@ Router.onRouteChangeError = () => NProgress.done();
 const Navbar = ({ isOpen, toggleMenu }) => {
   return (
     <div>
-      <Head>
-        <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
-      </Head>
-      <div>
-        <div className={ isOpen ? "navbar open" : "navbar" }>
-            <ul className="navbar__links">
-              <li className="navbar__item">
-                <Link prefetch href="/">
-                  <Chicken className="logo" width={200} height={100}/>
+      <div className={ isOpen ? "navbar open" : "navbar" }>
+          <ul className="navbar__links">
+            <li className="navbar__item">
+              <Link prefetch href="/">
+                <Chicken className="logo" width={200} height={100}/>
+              </Link>
+              <Menu />
+            </li>
+            <div className="navbar__navigation">
+              <li className="navbar__item" onClick={ toggleMenu }>
+                <Link activeClassName="active" prefetch href="/">
+                  <a className="navbar__link">
+                    <House className="navbar__item--svg" width={100} height={50} />
+                    <span className="navbar__item--text">Home</span>
+                  </a>
                 </Link>
-                <Menu />
               </li>
-              <div className="navbar__navigation">
-                <li className="navbar__item" onClick={ toggleMenu }>
-                  <Link activeClassName="active" prefetch href="/">
-                    <a className="navbar__link">
-                      <House className="navbar__item--svg" width={100} height={50} />
-                      <span className="navbar__item--text">Home</span>
-                    </a>
-                  </Link>
-                </li>
-                <li className="navbar__item" onClick={ toggleMenu }>
-                  <Link activeClassName="active" prefetch href="/work">
-                    <a className="navbar__link">
-                      <Smartphone className="navbar__item--svg" width={100} height={50} />
-                      <span className="navbar__item--text">Work</span>
-                    </a>
-                  </Link>
-                </li>
-                <li className="navbar__item" onClick={ toggleMenu }>
-                  <Link activeClassName="active" prefetch href="/connect">
-                    <a className="navbar__link">
-                      <Telephone className="navbar__item--svg" width={100} height={50} />
-                      <span className="navbar__item--text">Connect</span>
-                    </a>
-                  </Link>
-                </li>
-              </div>
-            </ul>
-        </div>
-        <Menu isOpen={isOpen} />
+              <li className="navbar__item" onClick={ toggleMenu }>
+                <Link activeClassName="active" prefetch href="/work">
+                  <a className="navbar__link">
+                    <Smartphone className="navbar__item--svg" width={100} height={50} />
+                    <span className="navbar__item--text">Work</span>
+                  </a>
+                </Link>
+              </li>
+              <li className="navbar__item" onClick={ toggleMenu }>
+                <Link activeClassName="active" prefetch href="/connect">
+                  <a className="navbar__link">
+                    <Telephone className="navbar__item--svg" width={100} height={50} />
+                    <span className="navbar__item--text">Connect</span>
+                  </a>
+                </Link>
+              </li>
+            </div>
+          </ul>
       </div>
+      <Menu isOpen={isOpen} />
     </div>
   )
 }
